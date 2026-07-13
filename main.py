b28aa8e93e8633338729fa6651440c9ca3cfbd50
rajat430 <47021948+rajat430@users.noreply.github.com>
2026-07-13 10:37:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ parser.add_argument("--value", required=True, help="Input value")
 args = parser.parse_args()
 
 print(args.value)
+try:
+  with open(args.value,"w") as f:
+    f.write("hello world")
+except Exception as error:
+  print(f"there is some issue: {error}")
